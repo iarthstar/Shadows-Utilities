0c0d408c3b15f7f982c617bc54bc63eb8236a015
Arth K. Gajjar <iarthstar@gmail.com>
2019-08-23 19:49:33 +0530

    Added : Support for TextLayer, ImageLayer and GroupLayer
    * added support for textlayer, imagelayer and grouplayer i.e. across layers copy, cut, paste and delete shadows
    
    * refactored commands both in Main.purs and in commands directory

--- purescript/src/Main.purs
+++ purescript/src/Main.purs
@@ -1,115 +1,75 @@
-module Main where
+module Main 
+  ( copyOrCutShadows
+  , pasteOrRemoveShadows
+  ) where
 
-import Prelude (Unit, bind, discard, pure, unit, ($), (<$>), (>>=))
+import Prelude
 
-import Control.Monad.Except (runExcept)
-import Data.Array (head, length)
+import Data.Array (length, (!!))
 import Data.Either (Either(..))
 import Data.Maybe (Maybe(..))
-import Data.Traversable (traverse)
+import Data.Newtype (class Newtype, unwrap, wrap)
+import Data.Traversable (traverse_)
 import Effect (Effect)
-import Foreign (F, Foreign)
-import Foreign.Class (decode, encode)
+import Foreign.Class (class Encode)
+import Optic.Getter ((^.))
+import Optic.Lens (lens)
+import Optic.Types (Lens')
 import Sketch.Dom as Dom
 import Sketch.Settings as Settings
-import Sketch.Types (ArtboardLayer(..), GroupLayer(..), ImageLayer(..), Layer(..), Shadow, ShapeLayer(..), ShapeStyle(..), TextLayer(..))
+import Sketch.Types (Layer(..), Shadow)
 import Sketch.UI as UI
 
-foreign import setShadowsForLayerID :: String -> Foreign -> Effect Unit
-foreign import removeShadowsForLayerID :: String -> Effect Unit
-
-copyShadows :: Effect Unit
-copyShadows = do
-  Dom.selectedLayers >>= case _ of
+copyOrCutShadows :: String -> Effect Unit
+copyOrCutShadows action = Dom.selectedLayers >>= case _ of
     Left err -> UI.message "Something went wrong..."
-    Right layers -> do
-      case length layers of
-        0 -> UI.alert "No Selection" "Please select a layer and try again..."
-        1 -> do
-          case head layers of
-            Nothing -> UI.message "Something went wrong..."
-            Just (Shape (ShapeLayer sl)) -> do
-              case sl.shapeType of
-                "Rectangle" -> do
-                  let (ShapeStyle style) = sl.style
-                  case style.shadows of
-                    Nothing -> pure unit
-                    Just shadows -> do
-                      Settings.setGlobalSettingForKey "copied-shadows" $ encode shadows
-                _ -> pure unit
-            _ -> pure unit
-        _ -> UI.alert "More than one layer selected" "Please select only one layer and try again..."
-    
-pasteShadows :: Effect Unit
-pasteShadows = do
-  selection <- Dom.selectedLayers
-  fetchedShadows <- fetchShadows
-  case selection, fetchedShadows of
-    Left _, Left _-> UI.message "Something went wrong..."
-    Right layers, Right shadows -> do
-      case length layers of
-        0 -> UI.alert "No Selection" "Please select a layer and try again..."
-        _ -> do
-          _ <- traverse (applyShadow shadows) layers
-          pure unit
-    _, _ -> UI.message "Something went wrong..."
-  
+    Right layers -> case layers !! 0, length layers of
+        _                 , 0 -> UI.alert "No Selection" "Please select a layer and try again..."
+        Just (Shape layer), 1 -> performAction layer
+        Just (Text layer) , 1 -> performAction layer
+        Just (Image layer), 1 -> performAction layer
+        Just (Group layer), 1 -> performAction layer
+        _                 , 1 -> UI.message "Sketch doesn't support Shadows on selected layer"
+        _                 , _ -> UI.alert "More than one layer selected" "Please select only one layer and try again..."
   where
-    fetchShadows :: Effect (Either String (Array Shadow))
-    fetchShadows = do
-      (shadowsF :: F (Array Shadow)) <- decode <$> Settings.globalSettingForKey "copied-shadows" 
-      pure $ case runExcept shadowsF of
-        Left err -> Left "Error Fetching Shadows"  
-        Right shadows -> Right shadows
-
-    applyShadow :: Array Shadow -> Layer -> Effect Unit
-    applyShadow shadows layer = do
-      let id = case layer of
-            Text (TextLayer tl) -> tl.id
-            Image (ImageLayer il) -> il.id
-            Shape (ShapeLayer sl) -> sl.id
-            Group (GroupLayer gl) -> gl.id
-            Artboard (ArtboardLayer al) -> al.id
-      setShadowsForLayerID id (encode shadows)
-
-cutShadows :: Effect Unit
-cutShadows = do
-  Dom.selectedLayers >>= case _ of
-    Left err -> UI.message "Something went wrong..."
-    Right layers -> do
-      case length layers of
-        0 -> UI.alert "No Selection" "Please select a layer and try again..."
-        1 -> do
-          case head layers of
-            Nothing -> UI.message "Something went wrong..."
-            Just (Shape (ShapeLayer sl)) -> do
-              case sl.shapeType of
-                "Rectangle" -> do
-                  let (ShapeStyle style) = sl.style
-                  case style.shadows of
-                    Nothing -> pure unit
-                    Just shadows -> do
-                      Settings.setGlobalSettingForKey "copied-shadows" $ encode shadows
-                      removeShadowsForLayerID sl.id
-                _ -> pure unit
+    performAction :: forall a b c d e. Encode e => Newtype a {style :: b, id :: String | c} => Newtype b { shadows :: Maybe e | d} => a -> Effect Unit
+    performAction layer = case layer ^. _style ^. _shadows of
+        Nothing -> pure unit
+        Just shadows -> do
+          Settings.setGlobalSettingForKey "copied-shadows" shadows
+          case action of
+            "CUT" -> Dom.setPropsForLayerID (layer ^. _id) ["style", "shadows"] ([] :: Array Shadow)
             _ -> pure unit
-        _ -> UI.alert "More than one layer selected" "Please select only one layer and try again..."
 
-removeShadows :: Effect Unit
-removeShadows = do
-  Dom.selectedLayers >>= case _ of
+pasteOrRemoveShadows :: String -> Effect Unit
+pasteOrRemoveShadows action = Dom.selectedLayers >>= case _ of
     Left err -> UI.message "Something went wrong..."
-    Right layers -> do
-      case length layers of
+    Right layers  -> case length layers of
         0 -> UI.alert "No Selection" "Please select a layer and try again..."
-        _ -> do
-          _ <- traverse removeShadow layers
-          pure unit
+        _ -> case action of
+            "REMOVE" -> traverse_ (\l -> Dom.setPropsForLayerID (getLayerId l) ["style", "shadows"]  ([] :: Array Shadow)) layers
+            "PASTE" -> Settings.globalSettingForKey "copied-shadows" >>= case _ of
+                Left err -> UI.message "Something went wrong..."
+                Right (shadows :: Array Shadow) -> traverse_ (\l -> Dom.setPropsForLayerID (getLayerId l) ["style", "shadows"] shadows) layers
+            _ -> pure unit
   where
-    removeShadow :: Layer -> Effect Unit
-    removeShadow layer = removeShadowsForLayerID case layer of
-      Text (TextLayer tl) -> tl.id
-      Image (ImageLayer il) -> il.id
-      Shape (ShapeLayer sl) -> sl.id
-      Group (GroupLayer gl) -> gl.id
-      Artboard (ArtboardLayer al) -> al.id
+    getLayerId :: Layer -> String
+    getLayerId layer = case layer of
+        Text tl -> tl ^. _id
+        Image il -> il ^. _id
+        Shape sl -> sl ^. _id
+        Group gl -> gl ^. _id
+        Artboard al -> al ^. _id
+
+-- | Accessors 
+_shapeType :: forall a b c. Newtype a { shapeType :: b | c } => Lens' a b
+_shapeType = lens (unwrap >>> _.shapeType) (\oldRec newVal -> wrap ((unwrap oldRec) { shapeType = newVal }))
+
+_style :: forall a b c. Newtype a { style :: b | c } => Lens' a b
+_style = lens (unwrap >>> _.style) (\oldRec newVal -> wrap ((unwrap oldRec) { style = newVal }))
+
+_shadows :: forall a b c. Newtype a { shadows :: b | c } => Lens' a b
+_shadows = lens (unwrap >>> _.shadows) (\oldRec newVal -> wrap ((unwrap oldRec) { shadows = newVal }))
+
+_id :: forall a b c. Newtype a { id :: b | c } => Lens' a b
+_id = lens (unwrap >>> _.id) (\oldRec newVal -> wrap ((unwrap oldRec) { id = newVal }))
--- sketch-plugin/src/commands/copy-shadows.js
+++ sketch-plugin/src/commands/copy-shadows.js
@@ -1,3 +1,17 @@
-export default function () {
-    require('../scripts/output.js')["Main"].copyShadows();
+const PS = require('../scripts/output.js')["Main"];
+
+exports["copyShadows"] = function() {
+    PS.copyOrCutShadows("COPY")();
+}
+
+exports["cutShadows"] = function() {
+    PS.copyOrCutShadows("CUT")();
+}
+
+exports["pasteShadows"] = function() {
+    PS.pasteOrRemoveShadows("PASTE")();
+}
+
+exports["removeShadows"] = function() {
+    PS.pasteOrRemoveShadows("REMOVE")();
 }
