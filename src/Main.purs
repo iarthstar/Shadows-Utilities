e606a907f1a7d65f29b230fbb31be77e0c2f4d31
Arth K. Gajjar <iarthstar@gmail.com>
2020-02-03 03:02:04 +0530

    Refactor: removed uses of lenses

--- src/Main.purs
+++ src/Main.purs
@@ -3,57 +3,46 @@ module Main ( performAction ) where
 import Prelude
 
 import Data.Array (length, (!!))
-import Data.Either (Either(..), either)
+import Data.Either (Either(..))
 import Data.Foldable (traverse_)
 import Data.Maybe (Maybe(..), fromMaybe)
-import Data.Newtype (class Newtype, unwrap, wrap)
+import Data.Newtype (unwrap)
 import Effect (Effect)
-import Optic.Getter ((^.))
-import Optic.Lens (lens)
-import Optic.Types (Lens')
 import Sketch.Dom as Dom
 import Sketch.Settings as Settings
-import Sketch.Types (Layer(..), Shadow)
+import Sketch.Types (ArtboardLayer(..), GroupLayer(..), ImageLayer(..), Layer(..), Shadow, ShapeLayer(..), TextLayer(..))
 import Sketch.UI as UI
 
 performAction :: String -> Effect Unit
 performAction action = Dom.selectedLayers >>= case _ of
     Left err      -> UI.message "Something went wrong..."
     Right layers  -> case length layers, layers !! 0, action of
-        0, _                , _         -> UI.alert "No Selection" "Please select a layer and try again..."
-        1, Just (Artboard l), _         -> UI.message "Sketch doesn't support Shadows on selected layer"
-        1, Just layer       , "COPY"    -> Settings.setGlobalSettingForKey "Shadows-Utilities.copied-shadows" $ getShadows layer
-        1, Just layer       , "CUT"     -> (Settings.setGlobalSettingForKey "Shadows-Utilities.copied-shadows" <<< getShadows *> setShadows ([] :: Array Shadow)) layer
-        _, _                , "PASTE"   -> either (\_ -> UI.message "Something went wrong...") (\s -> traverse_ (setShadows s) layers) =<< Settings.globalSettingForKey "Shadows-Utilities.copied-shadows"
-        _, _                , "DELETE"  -> traverse_ (setShadows ([] :: Array Shadow)) layers
-        _, _                , "COPY"    -> UI.alert "More than one layer selected" "Please select only one layer and try again..."
-        _, _                , "CUT"     -> UI.alert "More than one layer selected" "Please select only one layer and try again..."
-        _, _                , _         -> pure unit
+        0, _                , _        -> UI.alert "No Selection" "Please select a layer and try again..."
+        1, Just (Artboard l), _        -> UI.message "Sketch doesn't support Shadows on selected layer"
+        1, Just layer       , "COPY"   -> Settings.setGlobalSettingForKey "Shadows-Utilities.copied-shadows" (getShadows layer)
+        1, Just layer       , "CUT"    -> Settings.setGlobalSettingForKey "Shadows-Utilities.copied-shadows" (getShadows layer) *> 
+                                          setShadows ([] :: Array Shadow) layer
+        _, _                , "PASTE"  -> Settings.globalSettingForKey "Shadows-Utilities.copied-shadows" >>= case _ of
+                                            Left  _ -> UI.message "Something went wrong..."
+                                            Right s -> traverse_ (setShadows s) layers
+        _, _                , "DELETE" -> traverse_ (setShadows ([] :: Array Shadow)) layers
+        _, _                , "COPY"   -> UI.alert "More than one layer selected" "Please select only one layer and try again..."
+        _, _                , "CUT"    -> UI.alert "More than one layer selected" "Please select only one layer and try again..."
+        _, _                , _        -> pure unit
 
-getLayerId :: Layer -> String
-getLayerId = case _ of
-    Text     l -> l ^. _id
-    Image    l -> l ^. _id
-    Shape    l -> l ^. _id
-    Group    l -> l ^. _id
-    Artboard l -> l ^. _id
+getID :: Layer -> String
+getID (Text     (TextLayer     l)) = l.id
+getID (Image    (ImageLayer    l)) = l.id
+getID (Group    (GroupLayer    l)) = l.id
+getID (Shape    (ShapeLayer    l)) = l.id
+getID (Artboard (ArtboardLayer l)) = l.id
 
 getShadows :: Layer -> Array Shadow
-getShadows = fromMaybe [] <<< case _ of
-        Text     l -> l ^. _style ^. _shadows
-        Image    l -> l ^. _style ^. _shadows
-        Shape    l -> l ^. _style ^. _shadows
-        Group    l -> l ^. _style ^. _shadows
-        Artboard l -> Just []
+getShadows (Text     (TextLayer     l)) = fromMaybe [] (unwrap l.style).shadows
+getShadows (Image    (ImageLayer    l)) = fromMaybe [] (unwrap l.style).shadows
+getShadows (Group    (GroupLayer    l)) = fromMaybe [] (unwrap l.style).shadows
+getShadows (Shape    (ShapeLayer    l)) = fromMaybe [] (unwrap l.style).shadows
+getShadows (Artboard (ArtboardLayer l)) = []
 
 setShadows :: Array Shadow -> Layer -> Effect Unit
-setShadows s l = Dom.setPropsForLayerID (getLayerId l) ["style", "shadows"] s
-
-_style :: forall a b c. Newtype a { style :: b | c } => Lens' a b
-_style = lens (unwrap >>> _.style) (\oldRec newVal -> wrap ((unwrap oldRec) { style = newVal }))
-
-_shadows :: forall a b c. Newtype a { shadows :: b | c } => Lens' a b
-_shadows = lens (unwrap >>> _.shadows) (\oldRec newVal -> wrap ((unwrap oldRec) { shadows = newVal }))
-
-_id :: forall a b c. Newtype a { id :: b | c } => Lens' a b
-_id = lens (unwrap >>> _.id) (\oldRec newVal -> wrap ((unwrap oldRec) { id = newVal }))
+setShadows s l = Dom.setPropsForLayerID (getID l) ["style", "shadows"] s
